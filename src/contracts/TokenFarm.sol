e6ccf18821e5978c4fcafbbe9d0684a555dfabfb
Harshit <hg25112002@gmail.com>
2023-03-09 16:44:12 +0530

    Transferred 1 million dapptoken to tokenFarm and 100 Mock DAI tokens to inverstor

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -4,4 +4,11 @@ import "./DappToken.sol";
 
 contract TokenFarm {
     string public name = "Dapp Token Farm";
+    DappToken public dappToken;
+    DaiToken public daiToken;
+
+    constructor(DappToken _dappToken, DaiToken _daiToken) public {
+        dappToken = _dappToken;
+        daiToken = _daiToken;
+    }
 }
