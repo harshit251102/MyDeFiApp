5e62ddf06ffa7eb7cbba73545da4d67ba27cec88
Harshit <hg25112002@gmail.com>
2023-03-10 13:34:41 +0530

    added a function issueTokens

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -6,6 +6,7 @@ contract TokenFarm {
     string public name = "Dapp Token Farm";
     DappToken public dappToken;
     DaiToken public daiToken;
+    address public owner;
 
     address[] public stakers;
     mapping(address => uint) public stakingBalance;
@@ -15,10 +16,14 @@ contract TokenFarm {
     constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken = _daiToken;
+        owner = msg.sender;
     }
 
     // Staking Tokens (Deposit)
     function stakeTokens(uint _amount) public {
+        // Require amount greater than 0
+        require(_amount > 0, "amount cannot be 0");
+
         // Transfer Mock Dai tokens to this contract for staking
         daiToken.transferFrom(msg.sender, address(this), _amount);
 
@@ -32,4 +37,20 @@ contract TokenFarm {
         }
     }
 
+    // Unstaking Tokens (Withdraw)
+
+    //Issuing Tokens
+    function issueTokens() public {
+        // Only owner can call this function
+        require(msg.sender == owner, "caller must be the owner");
+
+        // Issue tokens to all stakers
+        for (uint i=0; i<stakers.length; i++) {
+            address recipient = stakers[i];
+            uint balance = stakingBalance[recipient];
+            if (balance > 0) {
+                dappToken.transfer(recipient, balance);
+            }
+        }
+    }
 }
