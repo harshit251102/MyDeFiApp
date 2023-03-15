419e9dcde412437653c61bddf312b046c34169c0
Harshit <hg25112002@gmail.com>
2023-03-15 16:07:53 +0530

    established connection between my blockchain and site

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react'
 import Navbar from './Navbar'
 import './App.css'
-
+import Web3 from 'web3'
 class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  } 
 
   constructor(props) {
     super(props)
